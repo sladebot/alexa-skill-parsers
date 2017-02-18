e129c8b45518f8240e53ca58f31cfefd201a4c10
Souranil Sen <souranil@gmail.com>
2017-02-18 06:41:00 -0500

    Adding intents

---
+++ index.js
@@ -0,0 +1,69 @@
+let express = require("express"),
+  bodyParser = require("body-parser"),
+  alexaVerifier = require("alexa-verifier"),
+  app = express();
+
+const workouts = [
+  'Push Ups',
+  'Sit Ups',
+  'Squats',
+  'Power Jump',
+  'Crunches',
+  'Burpees'
+];
+
+app.use(bodyParser.json({
+  verify: function getRawBody(req, res, buf) {
+    req.rawBody = buf.toString()
+  }
+}))
+
+app.post("/getWorkouts", requestVerifier, (req, res) => {
+  console.log(req);
+  if (req.body.request.type === 'LaunchRequest') {
+    res.json({
+      "version": "1.0",
+      "response": {
+        "shouldEndSession": true,
+        "outputSpeech": {
+          "type": "SSML",
+          "ssml": "<speak>Hmm <break time=\"1s\"/> Do you want to know today's workouts ?</speak>"
+        }
+      }
+    });
+  } else if(req.body.request.type === "SessionEndedRequest") {
+    console.log("Session ended", req.body.request.reason);
+  } else if(req.body.request.type === "IntentRequest") {
+    res.json({
+      "version": "1.0",
+      "response": {
+        "shouldEndSession": true,
+        "outputSpeech": {
+          "type": "SSML",
+          "ssml": "<speak>Let me list down the workouts for today!</speak>"
+        }
+      }
+    })
+  }
+});
+
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + server.address().port);
+});
+
+function requestVerifier(req, res, next) {
+  alexaVerifier(
+    req.headers.signaturecertchainurl,
+    req.headers.signature,
+    req.rawBody,
+    function verificationCallback(err) {
+      if (err) {
+        res.status(401).json({message: 'Verification Failure', error: err})
+      } else {
+        next()
+      }
+    }
+  )
+}
