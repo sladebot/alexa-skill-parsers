d53e516e9302bab28ddd9497d27c3e6496e62c3e
Souranil Sen <souranil@gmail.com>
2017-02-18 14:32:46 -0500

    Heroku push

--- index.js
+++ index.js
@@ -19,8 +19,6 @@ app.use(bodyParser.json({
   }
 }));
 
-
-
 app.post('/api/alexa', (req, res) => {
   console.log("Echo request");
   const context = {
@@ -35,53 +33,6 @@ app.post('/api/alexa', (req, res) => {
   };
   req.body.request.locale = "en-GB";
   alexa.handler(req.body, context);
-  res.status(200).json({"status": 200});
-})
-
-// app.post("/getWorkouts", requestVerifier, (req, res) => {
-//   console.log(req);
-//   if (req.body.request.type === 'LaunchRequest') {
-//     res.json({
-//       "version": "1.0",
-//       "response": {
-//         "shouldEndSession": true,
-//         "outputSpeech": {
-//           "type": "SSML",
-//           "ssml": "<speak>Hmm <break time=\"1s\"/> Do you want to know today's workouts ?</speak>"
-//         }
-//       }
-//     });
-//   } else if(req.body.request.type === "SessionEndedRequest") {
-//     console.log("Session ended", req.body.request.reason);
-//   } else if(req.body.request.type === "IntentRequest") {
-//     res.json({
-//       "version": "1.0",
-//       "response": {
-//         "shouldEndSession": true,
-//         "outputSpeech": {
-//           "type": "SSML",
-//           "ssml": "<speak>Let me list down the workouts for today!</speak>"
-//         }
-//       }
-//     })
-//   }
-// });
-
-
-
-// function requestVerifier(req, res, next) {
-//   alexaVerifier(
-//     req.headers.signaturecertchainurl,
-//     req.headers.signature,
-//     req.rawBody,
-//     function verificationCallback(err) {
-//       if (err) {
-//         res.status(401).json({message: 'Verification Failure', error: err})
-//       } else {
-//         next()
-//       }
-//     }
-//   )
-// }
+});
 
 module.exports = app;
