17c9c5cba7b85c3ef3a4db79bd18930ab5ed6add
Souranil Sen <souranil@gmail.com>
2017-02-18 06:01:52 -0500

    Adding new route

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 
+const workouts = [
+  'Push Ups',
+  'Sit Ups',
+  'Squats',
+  'Power Jump',
+  'Crunches',
+  'Burpees'
+];
+
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/getWorkouts', function(req, res) {
+  res.status(200).json({"status": "OK"});
+});
+
 
 module.exports = router;
