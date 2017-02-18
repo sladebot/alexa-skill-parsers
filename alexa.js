36b90c9b6671549fa222243bc85726262d6ec005
Souranil Sen <souranil@gmail.com>
2017-02-18 08:07:07 -0500

    removing translation

--- alexa.js
+++ alexa.js
@@ -3,35 +3,31 @@
 var Alexa = require("alexa-sdk");
 const APP_ID = "amzn1.ask.skill.8985b048-54ab-452c-bee1-b9a0bd93603d";
 
-const languageStrings = {
-    'en-GB': {
-        translation: {
-            WORKOUTS: [
-                'Push Ups',
-                'Sit Ups',
-                'Squats',
-                'Power Jump',
-                'Crunches',
-                'Burpees'
-            ],
-            SKILL_NAME: 'Workouts',
-            GET_WORKOUT_MESSAGE: "Here are your workouts options: ",
-            HELP_MESSAGE: 'You can say tell me workouts, or, you can say exit... What can I help you with?',
-            HELP_REPROMPT: 'What can I help you with?',
-            STOP_MESSAGE: 'Goodbye!',
-        },
-    }
-};
+const constants = {
+    WORKOUTS: [
+        'Push Ups',
+        'Sit Ups',
+        'Squats',
+        'Power Jump',
+        'Crunches',
+        'Burpees'
+    ],
+    SKILL_NAME: 'Workouts',
+    GET_WORKOUT_MESSAGE: "Here are your workouts options: ",
+    HELP_MESSAGE: 'You can say tell me workouts, or, you can say exit... What can I help you with?',
+    HELP_REPROMPT: 'What can I help you with?',
+    STOP_MESSAGE: 'Goodbye!'
+}
 
 const handlers = {
     'LaunchRequest': function () {
         this.emit('GetWorkouts');
     },
     'GetNewWorkoutIntent': function () {
-        this.emit('GetWorkouts');
+        this.emit('GetWorkout');
     },
     'GetWorkouts': function () {
-        const workoutList = this.t('WORKOUTS');
+        const workoutList = constants.WORKOUTS;
         let workoutListMessage = []
         console.log(workoutList);
         workoutList.forEach((_workout, index) => {
@@ -43,7 +39,8 @@ const handlers = {
         this.emit(':tellWithCard', speechOutput);
     },
     'GetWorkout': function(intent) {
-        const workoutList = this.t('WORKOUTS');
+        const workoutList = constants.WORKOUTS;
+        console.log(workoutList)
         let workout = null;
         
         workoutList.forEach((_workout, _index) => {
